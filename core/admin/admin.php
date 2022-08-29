63a6240c4c43d95b93ad8085d45eb7538f4489b9
Rémi <71976102+remi-bourgougnon@users.noreply.github.com>
2022-08-29 11:01:36 +0200

    Must fix the image width. The page also reloads when clicking on the button. must fix that. The plugin can now download and activate plugins when clicking on the "install pack" button.

--- core/admin/admin.php
+++ core/admin/admin.php
@@ -1,14 +1,10 @@
 <?php
 
+//enqueue the css file
 wp_enqueue_style('admin.css', '/wp-content/plugins/plug-packet/assets/css/admin.css');
+wp_enqueue_script('admin.js', '/wp-content/plugins/plug-packet/assets/js/admin.js');
 
-function plug_packet_settings_init()
-{
-    register_setting('plugpacket', 'plugpacket_options');
-}
-
-add_action('admin_init', 'plug_packet_settings_init');
-
+//add the top level menu
 function plug_packet_options_page()
 {
     add_menu_page(
@@ -16,43 +12,81 @@ function plug_packet_options_page()
         'PlugPacket',
         'manage_options',
         'plugpacket',
-        'plug_options_page_html'
+        'plugin_packs_options_page_html'
     );
 }
 
 add_action('admin_menu', 'plug_packet_options_page');
 
-function plug_options_page_html()
+//The plugin packs function.
+function plugin_packs_options_page_html()
 {
+    $x = 0;
+
+    //create different variable for the title, images etc of the plugin packs
+    $plugin_pack_titles = array(
+        '0' => 'Basic Pack',
+        '1' => 'Premium Pack',
+        '2' => 'Premium Pack 2',
+        '3' => 'test'
+    );
+
+    $plugin_pack_types = array(
+        '0' => 'essentials',
+        '1' => 'ecommerce',
+        '2' => 'blog',
+        '3' => 'test'
+    );
+
+    $plugin_pack_images = array(
+        '0' => 'plug-packet/assets/images/cat.jpeg',
+        '1' => 'plug-packet/assets/images/cat.jpeg',
+        '2' => 'plug-packet/assets/images/cat.jpeg',
+        '3' => 'plug-packet/assets/images/cat.jpeg',
+    );
+
+    $plugin_pack_plugins = array(
+    );
+
+    //function that is executed when the button is pressed. installs the plugins
+    if (isset($_POST['install-pack-button'])) {
+        require_once plugin_dir_path(__FILE__) . 'plugin-installer/plugin-installer.php';
+        $plugin_pack_installer = new Plug_Packet_Plugin_Installer();
+        foreach ($plugin_pack_plugins as $value) {
+            $plugin_pack_installer->plugin_packs_installer($plugin_pack_plugins[$x]);
+            $x = $x + 1;
+        }
+    }
+
+    //function that is executed when the button is pressed. displays more information.
+    if (isset($_POST['more-information-button'])) {
+        echo "This is Button2 that is selected";
+    }
     ?>
     <div class="plugin-packs">
-        <div class="plugin-pack">
-            <div class="plugin-pack-image"><img src=""></div>
-            <div class="plugin-pack-title">test</div>
-            <div class="plugin-pack-creator">by test</div>
-            <div class="plugin-pack-buttons">
-                <button class="install-pack-button">Install Pack</button>
-                <button class="more-information-button">More info</button>
+        <?php
+        foreach ($plugin_pack_titles as $value) {
+            //the code that displays the plugin pack
+            ?>
+            <div class="plugin-pack">
+                <div class="plugin-pack-image"><img src="/wp-content/plugins/<?php echo $plugin_pack_images[$x] ?>">
+                </div>
+                <div class="plugin-pack-title"><?php echo $plugin_pack_titles[$x] ?></div>
+                <div class="plugin-pack-types"><?php echo $plugin_pack_types[$x] ?></div>
+                <div class="plugin-pack-buttons">
+                    <form method="post">
+                        <input class="install-pack-button" type="submit" name="install-pack-button"
+                               value="Install Pack"/>
+
+                        <input class="more-information-button" type="submit" name="more-information-button"
+                               value="More info"/>
+                    </form>
+                </div>
             </div>
-        </div>
-        <div class="plugin-pack">
-            <div class="plugin-pack-image"><img src="/wp-content/plugins/mountain.jpeg"></div>
-            <div class="plugin-pack-title">test</div>
-            <div class="plugin-pack-creator">by test</div>
-            <div class="plugin-pack-buttons">
-                <button class="install-pack-button">Install Pack</button>
-                <button class="more-information-button">More info</button>
-            </div>
-        </div>
-        <div class="plugin-pack">
-            <div class="plugin-pack-image"><img src="/wp-content/plugins/mountain.jpeg"></div>
-            <div class="plugin-pack-title">test</div>
-            <div class="plugin-pack-creator">by test</div>
-            <div class="plugin-pack-buttons">
-                <button class="install-pack-button">Install Pack</button>
-                <button class="more-information-button">More info</button>
-            </div>
-        </div>
+            <?php
+            $x = $x + 1;
+        }
+        ?>
     </div>
     <?php
 }
---
+++ core/admin/plugin-installer/plugin-installer.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once(ABSPATH . 'wp-admin/includes/plugin-install.php');
+require_once(ABSPATH . 'wp-admin/includes/class-wp-upgrader.php');
+require_once(ABSPATH . 'wp-admin/includes/class-wp-ajax-upgrader-skin.php');
+require_once(ABSPATH . 'wp-admin/includes/class-plugin-upgrader.php');
+require_once(ABSPATH . '/wp-admin/includes/plugin.php');
+
+
+class Plug_Packet_Plugin_Installer
+{
+
+    function plugin_packs_installer($plugins)
+    {
+        $wordpress_api = plugins_api('plugin_information',
+            array(
+                'slug' => $plugins
+            )
+        );
+        $ajax_upgrader_skin = new WP_Ajax_Upgrader_Skin();
+        $plugin_upgrader = new Plugin_Upgrader($ajax_upgrader_skin);
+        $plugin_upgrader->install($wordpress_api->download_link);
+
+        readfile($plugins . '/' . $plugins . '.php');
+        activate_plugin($plugins . '/' . $plugins . '.php');
+    }
+}
