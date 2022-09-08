926ee0e47da532bcbaac62a17b22f950a60c9362
Rémi <71976102+remi-bourgougnon@users.noreply.github.com>
2022-09-08 15:41:13 +0200

    Deleted the button that removes the plugin. Modified the plugin packs array and replaced the 'type' with a plugin list.

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -3,38 +3,10 @@ jQuery(document).ready(function($) {
         jQuery.post(ajaxurl, { action: "plugin_pack_installer"}, function() {
             if(jQuery('.install-pack-button').find('i').hasClass("fa-refresh fa-spin")) {
                 jQuery('.install-pack-button').find('i').addClass("fa-download").removeClass("fa-refresh fa-spin");
-                jQuery('.install-pack-button').css('display', 'none');
-                jQuery('.remove-pack-button').css('display', 'inline-block');
-                localStorage.setItem('button_clicked', '1');
             }
         });
         if(jQuery(this).find('i').hasClass("fa-download")) {
             jQuery(this).find('i').addClass("fa-refresh fa-spin").removeClass("fa-download");
         }
     });
-
-    jQuery(".remove-pack-button").click(function(){
-        jQuery.post(ajaxurl, { action: "plugin_pack_uninstaller"}, function() {
-            if(jQuery('.remove-pack-button').find('i').hasClass("fa-refresh fa-spin")) {
-                jQuery('.remove-pack-button').find('i').addClass("fa-trash").removeClass("fa-refresh fa-spin");
-                jQuery('.install-pack-button').css('display', 'inline-block');
-                jQuery('.remove-pack-button').css('display', 'none');
-                localStorage.setItem('button_clicked', '0');
-            }
-        });
-        if(jQuery('.remove-pack-button').find('i').hasClass("fa-trash"))
-            jQuery('.remove-pack-button').find('i').addClass("fa-refresh fa-spin").removeClass("fa-trash");
-    });
-
-    if(localStorage.getItem('button_clicked') === '1')
-    {
-        jQuery('.install-pack-button').css('display', 'none');
-        jQuery('.remove-pack-button').css('display', 'inline-block');
-    }
-
-    else if(localStorage.getItem('button_clicked') === '0')
-    {
-        jQuery('.install-pack-button').css('display', 'inline-block');
-        jQuery('.remove-pack-button').css('display', 'none');
-    }
 })
--- core/admin/admin.php
+++ core/admin/admin.php
@@ -23,45 +23,45 @@ add_action('admin_menu', 'plug_packet_options_page');
 //The plugin packs function.
 function plugin_packs_options_page_html()
 {
-    $x = 0;
+    $plugin_packs = [
+        'Beginner Pack' => [
+            'title' => 'Basic Pack',
+            'list' => [
+                'Elementor Website Builder',
+                'UpdraftPlus WordPress Backup Plugin',
+                'Really Simple SSL'
+            ],
+            'image' => 'plug-packet/assets/images/image_pack.png'
+        ],
+        'Test Pack' => [
+            'title' => 'Test Pack',
+            'list' => [
+                'Beaver Builder – WordPress Page Builder'
+            ],
+            'image' => 'plug-packet/assets/images/image_pack.png'
+        ]
+    ];
 
-    //create different variable for the title, images etc of the plugin packs
-    $plugin_pack_titles = array(
-        '0' => 'Basic Pack',
-    );
-
-    $plugin_pack_types = array(
-        '0' => 'essentials',
-    );
-
-    $plugin_pack_images = array(
-        '0' => 'plug-packet/assets/images/image_pack.png',
-    );
-
-    //function that is executed when the button is pressed. displays more information.
-    if (isset($_POST['more-information-button'])) {
-        echo "This is Button2 that is selected";
-    }
     ?>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <div class="plugin-packs">
         <?php
-        foreach ($plugin_pack_titles as $value) {
+        foreach ($plugin_packs as $plugin_pack) {
             //the code that displays the plugin pack
             ?>
             <div class="plugin-pack">
-                <div class="plugin-pack-image"><img src="/wp-content/plugins/<?php echo $plugin_pack_images[$x] ?>">
+                <div class="plugin-pack-image"><img src="/wp-content/plugins/<?php echo $plugin_pack['image'] ?>">
                 </div>
-                <div class="plugin-pack-title"><?php echo $plugin_pack_titles[$x] ?></div>
-                <div class="plugin-pack-types"><?php echo $plugin_pack_types[$x] ?></div>
+                <div class="plugin-pack-title"><?php echo $plugin_pack['title'] ?></div>
+                <div class="plugin-pack-list"><?php foreach ($plugin_pack['list'] as $plugin_list) {
+                        echo '- ' . $plugin_list . '<br>';
+                    } ?></div>
                 <div class="plugin-pack-buttons">
-                    <button class="install-pack-button"><i class="fa fa-download"></i> Install Pack</button>
-                    <button class="remove-pack-button"><i class="fa fa-trash"></i> Remove Pack</button>
-                    <button class="more-information-button">More info</button>
+                    <button class="install-pack-button"><i class="fa fa-download"></i> Install and Activate Pack
+                    </button>
                 </div>
             </div>
             <?php
-            $x = $x + 1;
         }
         ?>
     </div>
@@ -69,7 +69,8 @@ function plugin_packs_options_page_html()
 }
 
 //function that is executed when the button is pressed. installs the plugins
-function plugin_pack_installer () {
+function plugin_pack_installer()
+{
     $plugin_pack_plugins = array(
         '0' => 'elementor',
         '1' => 'updraftplus',
@@ -84,23 +85,5 @@ function plugin_pack_installer () {
     }
     wp_die();
 }
-add_action( 'wp_ajax_plugin_pack_installer', 'plugin_pack_installer' );
 
-
-
-function plugin_pack_uninstaller () {
-    $plugin_pack_plugins = array(
-        '0' => 'elementor',
-        '1' => 'updraftplus',
-        '2' => 'really-simple-ssl',
-    );
-    $x = 0;
-    require_once plugin_dir_path(__FILE__) . 'plugin-installer/plugin-uninstaller.php';
-    $plugin_pack_uninstaller = new Plug_Packet_Plugin_Uninstaller();
-    foreach ($plugin_pack_plugins as $value) {
-        $plugin_pack_uninstaller->plugin_packs_uninstaller($plugin_pack_plugins[$x]);
-        $x = $x + 1;
-    }
-    wp_die();
-}
-add_action( 'wp_ajax_plugin_pack_uninstaller', 'plugin_pack_uninstaller' );
+add_action('wp_ajax_plugin_pack_installer', 'plugin_pack_installer');
