92bd8bb267012f9af1d8101c2dd36d8135601ea8
Rémi <71976102+remi-bourgougnon@users.noreply.github.com>
2022-09-27 16:00:25 +0200

    an icon indicates if you can download a plugin or not. it prevents someone from trying to download two plugins at once.

--- src/assets/js/plp-admin.js
+++ src/assets/js/plp-admin.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function ($) {
 
-    function call_ajax(index, pack_plugins, button_clicked_icon) {
+    function call_ajax(index, pack_plugins, button_clicked_icon, plugin_icons) {
         let pack_plugin = pack_plugins[index];
 
         $.post(ajaxurl, {
@@ -21,13 +21,16 @@ jQuery(document).ready(function ($) {
             }
 
             if (index < pack_plugins.length - 1) {
-                call_ajax(++index, pack_plugins, button_clicked_icon);
+                call_ajax(++index, pack_plugins, button_clicked_icon, plugin_icons);
             } else {
                 // Stop loading icon when every plugin ajax is done
                 if (button_clicked_icon !== null) {
                     button_clicked_icon.addClass("fa-download").removeClass("fa-refresh fa-spin");
+                    $('.plp-install-pack-button').prop('disabled', false);
                 }
                 $('.plp-install-pack-button-disabled').prop('disabled', false).addClass('plp-install-pack-button').removeClass('plp-install-pack-button-disabled');
+                $('.plp-plugin-icon-empty-circle').not(plugin_icons).show();
+                $('.plp-plugin-icon-disabled').not(plugin_icons).hide();
             }
 
         });
@@ -41,25 +44,28 @@ jQuery(document).ready(function ($) {
         // Loading icons
         button_clicked_icon.addClass("fa-refresh fa-spin").removeClass("fa-download");
         plugin_icons.addClass("fa-refresh fa-spin").removeClass("fa-check-circle").removeClass("fa-circle-o");
+        $('.plp-plugin-icon-empty-circle').not(plugin_icons).hide();
+        $('.plp-plugin-icon-empty-circle').not(plugin_icons).closest('li').find('.plp-plugin-icon-disabled').show();
 
         // Call ajax on each plugin synchronously and recursively
         call_ajax(0, pack_plugins, button_clicked_icon);
 
-        $('button').prop('disabled', true).addClass('plp-install-pack-button-disabled').removeClass('plp-install-pack-button');
+        $('button').not(this).prop('disabled', true).addClass('plp-install-pack-button-disabled').removeClass('plp-install-pack-button');
+        $('.plp-install-pack-button').prop('disabled', true);
     });
 
     $(".plp-plugin-icon-empty-circle").click(function () {
         let pack_plugins = [$(this).parent('li').data('plp-pack-plugin')];
         let plugin_icons = $(this);
 
-        console.log(pack_plugins);
-
         // Loading icons
         plugin_icons.addClass("fa-refresh fa-spin").removeClass("fa-circle-o");
+        $('.plp-plugin-icon-empty-circle').not(plugin_icons).hide();
+        $('.plp-plugin-icon-empty-circle').not(plugin_icons).closest('li').find('.plp-plugin-icon-disabled').show();
 
         // Call ajax on each plugin synchronously and recursively
-        call_ajax(0, pack_plugins, null);
+        call_ajax(0, pack_plugins, null, plugin_icons);
 
-        $('button').not(this).prop('disabled', true).addClass('plp-install-pack-button-disabled').removeClass('plp-install-pack-button');
+        $('button').prop('disabled', true).addClass('plp-install-pack-button-disabled').removeClass('plp-install-pack-button');
     });
 });
