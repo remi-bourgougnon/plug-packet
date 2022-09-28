03237307da78f6ee3cb90d14a4ccb381ac390935
Rémi <71976102+remi-bourgougnon@users.noreply.github.com>
2022-09-28 10:42:58 +0200

    Formatted lines.

--- plug-packet.php
+++ plug-packet.php
@@ -11,17 +11,15 @@
  * Update URI:        to do
  * Text Domain:       to do
  * Domain Path:       to do
-
  * PlugPacket is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
  * any later version.
-
  * PlugPacket is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
-*/
+ */
 
 use plp\core\admin\Plp_Admin;
 
@@ -29,7 +27,8 @@ use plp\core\admin\Plp_Admin;
 /**
  * The function is fired after WordPress has loaded. the action includes are admin.php file which is the main php file for the plugin admin settings page.
  */
-function plp_settings_page() {
+function plp_settings_page()
+{
     /**
      * PSR-4 autoloader from composer
      */
@@ -40,13 +39,16 @@ function plp_settings_page() {
      */
     new Plp_Admin();
 }
-add_action( 'init', 'plp_settings_page' );
+
+add_action('init', 'plp_settings_page');
 
 
 /**
  * Sets the activation hook for the plugin. The function that we created above is then called.
  */
-function plp_activate() {
+function plp_activate()
+{
     plp_settings_page();
 }
-register_activation_hook( __FILE__, 'plp_activate' );
+
+register_activation_hook(__FILE__, 'plp_activate');
